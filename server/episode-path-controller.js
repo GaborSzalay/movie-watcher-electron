948f8cc7213a5dd7a29b68008c04027151ce783f
GaborSzalay <yaastreet@gmail.com>
2017-04-24 03:28:07 +0200

    starting vlc child process by choosing a movie

--- server/episode-path-controller.js
+++ server/episode-path-controller.js
@@ -1,11 +1,19 @@
+const child_process = require('child_process');
+const storage = require.main.require('./server/watcher-storage.js');
+
 module.exports = (() => {
     const episodePath = document.getElementById("episode-path");
 
     const handleEpisodeInputChange = (event) => {
         const files = event.path[0].files;
+        let vlcPathPromise;
 
         if (files.length > 0) {
-            console.log(files[0]);
+            vlcPathPromise = storage.getVlcPath();
+            vlcPathPromise.then((vlcPath) => {
+                child_process.execFile(vlcPath, [files[0].path]);
+                console.log(files[0]);
+            });
         }
     };
 
