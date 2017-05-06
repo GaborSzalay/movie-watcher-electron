34560c6ea8796b22118216053e5cc3ff4756ce53
GaborSzalay <yaastreet@gmail.com>
2017-05-06 23:59:18 +0200

    changing

--- server/episode-path-controller.js
+++ server/episode-path-controller.js
@@ -1,29 +1,27 @@
 const child_process = require('child_process');
 const storage = require.main.require('./server/watcher-storage.js');
 
-module.exports = (() => {
-    const episodePath = document.getElementById("episode-path");
+const episodePath = document.getElementById("episode-path");
 
-    const handleEpisodeInputChange = (event) => {
-        const files = event.path[0].files;
-        let vlcPathPromise;
+const handleEpisodeInputChange = (event) => {
+    const files = event.path[0].files;
+    let vlcPathPromise;
 
-        if (files.length > 0) {
-            vlcPathPromise = storage.getVlcPath();
-            vlcPathPromise.then((vlcPath) => {
-                child_process.execFile(vlcPath, [files[0].path, 'vlc://quit'], () => {
-                    console.log('vlc terminated');
-                });
-                console.log(files[0]);
+    if (files.length > 0) {
+        vlcPathPromise = storage.getVlcPath();
+        vlcPathPromise.then((vlcPath) => {
+            child_process.execFile(vlcPath, [files[0].path, 'vlc://quit'], () => {
+                console.log('vlc terminated');
             });
-        }
-    };
-
-    const init = () => {
-        episodePath.addEventListener("change", handleEpisodeInputChange)
-    };
-
-    return {
-        init: init
+            console.log(files[0]);
+        });
     }
-})();
+};
+
+const init = () => {
+    episodePath.addEventListener("change", handleEpisodeInputChange)
+};
+
+module.exports = {
+    init
+}
