abeef50db51d44f83335321c221b8fb769695194
GaborSzalay <yaastreet@gmail.com>
2017-05-06 23:58:23 +0200

    changing from module pattern vlc controller

--- server/vlc-path-controller.js
+++ server/vlc-path-controller.js
@@ -1,32 +1,30 @@
 const storage = require.main.require('./server/watcher-storage.js');
 
-module.exports = (() => {
-    const vlcPathElement = document.getElementById("vlc-path");
-    const vlcPathContainer = document.getElementById("vlc-path-container");
+const vlcPathElement = document.getElementById("vlc-path");
+const vlcPathContainer = document.getElementById("vlc-path-container");
 
-    const checkExistingVlcPath = () => {
-        const vlcPathPromise = storage.getVlcPath();
+const checkExistingVlcPath = () => {
+    const vlcPathPromise = storage.getVlcPath();
 
-        vlcPathPromise.then((vlcPath) => {
-            vlcPathContainer.textContent = vlcPath;
-        });
-    };
+    vlcPathPromise.then((vlcPath) => {
+        vlcPathContainer.textContent = vlcPath;
+    });
+};
 
-    const handleVlcInputChange = (event) => {
-        const files = event.path[0].files;
+const handleVlcInputChange = (event) => {
+    const files = event.path[0].files;
 
-        if (files.length > 0) {
-            vlcPathContainer.textContent = files[0].path;
-            storage.setVlcPath(files[0].path);
-        }
-    };
-
-    const init = () => {
-        checkExistingVlcPath();
-        vlcPathElement.addEventListener("change", handleVlcInputChange);
-    };
-
-    return {
-        init: init
+    if (files.length > 0) {
+        vlcPathContainer.textContent = files[0].path;
+        storage.setVlcPath(files[0].path);
     }
-})();
+};
+
+const init = () => {
+    checkExistingVlcPath();
+    vlcPathElement.addEventListener("change", handleVlcInputChange);
+};
+
+module.exports = {
+    init
+}
