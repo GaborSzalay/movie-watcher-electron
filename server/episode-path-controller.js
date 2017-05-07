63698755ca21d39ea0b98066a3406db77d73441f
GaborSzalay <yaastreet@gmail.com>
2017-05-07 21:59:59 +0200

    Adding recursive directory listing with checkboxes. hell yeah.

--- server/episode-path-controller.js
+++ server/episode-path-controller.js
@@ -1,20 +1,34 @@
-const child_process = require('child_process');
-const storage = require.main.require('./server/watcher-storage.js');
+const path = require('path');
 
 const episodePath = document.getElementById("episode-path");
+const rootDirectory = document.getElementById("root-directory");
+
+const getDir = (filePath) => {
+    return path.dirname(filePath.toString());
+}
+
+const getPossibleDirs = (filePath, _directories) => {
+    const directories = _directories ? _directories : [];
+    const currentDirectory = getDir(filePath);
+
+    if (filePath !== currentDirectory) {
+        directories.push(currentDirectory);
+        getPossibleDirs(currentDirectory, directories);
+    }
+    return directories;
+}
 
 const handleEpisodeInputChange = (event) => {
     const files = event.path[0].files;
     let vlcPathPromise;
 
     if (files.length > 0) {
-        vlcPathPromise = storage.getVlcPath();
-        vlcPathPromise.then((vlcPath) => {
-            child_process.execFile(vlcPath, [files[0].path, 'vlc://quit'], () => {
-                console.log('vlc terminated');
-            });
-            console.log(files[0]);
+        const filePath = files[0].path;
+        let options = '';
+        getPossibleDirs(filePath).forEach((directory, index) => {
+            options = options.concat(`<label><input type="radio" name="option" value="${index}">${directory}</label>`);
         });
+        rootDirectory.innerHTML = options;
     }
 };
 
