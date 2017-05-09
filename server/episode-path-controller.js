a0c6685f6908dd9fe2dda174f2e0b992aa19c4c2
Gabor Szalay <yaastreet@gmail.com>
2017-05-09 19:09:04 +0200

    deleting directory listing since there an easier way in chrome to get directory

--- server/episode-path-controller.js
+++ server/episode-path-controller.js
@@ -1,35 +1,5 @@
-const path = require('path');
-
 const episodePath = document.getElementById("episode-path");
 const rootDirectory = document.getElementById("root-directory");
-
-const getDir = (filePath) => {
-    return path.dirname(filePath.toString());
-}
-
-const addFilesRecursively = (rootDirectory) => {
-    console.log(rootDirectory);
-}
-
-const getPossibleDirs = (filePath, _directories) => {
-    const directories = _directories ? _directories : [];
-    const currentDirectory = getDir(filePath);
-
-    if (filePath !== currentDirectory) {
-        directories.push(currentDirectory);
-        getPossibleDirs(currentDirectory, directories);
-    }
-    return directories;
-}
-
-const registerOptionListeners = () => {
-    const inputs = rootDirectory.getElementsByTagName('input');
-    for (let counter = 0; counter < inputs.length; counter++) {
-        inputs[counter].addEventListener('change', () => {
-            addFilesRecursively(inputs[counter]);
-        });
-    }
-}
 
 const handleEpisodeInputChange = (event) => {
     const files = event.path[0].files;
@@ -37,12 +7,7 @@ const handleEpisodeInputChange = (event) => {
 
     if (files.length > 0) {
         const filePath = files[0].path;
-        let options = '';
-        getPossibleDirs(filePath).forEach((directory, index) => {
-            options = options.concat(`<label><input type="radio" name="option" value="${index}">${directory}</label>`);
-        });
-        rootDirectory.innerHTML = options;
-        registerOptionListeners();
+        console.log(filePath);
     }
 };
 
