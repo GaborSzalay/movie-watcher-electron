cfdcb62c9845de301878b583ea00adfd167ea2f5
GaborSzalay <yaastreet@gmail.com>
2017-05-15 21:44:50 +0200

    reading files recursively

--- server/episode-path-controller.js
+++ server/episode-path-controller.js
@@ -1,13 +1,25 @@
+const readdir = require('recursive-readdir-sync');
 const episodePath = document.getElementById("episode-path");
 
 const handleEpisodeInputChange = (event) => {
-    const files = event.path[0].files;
-    let vlcPathPromise;
+    const filesInput = event.path[0].files;
+    let files;
 
-    if (files.length > 0) {
-        const filePath = files[0].path;
+    if (filesInput.length > 0) {
+        const filePath = filesInput[0].path;
         console.log(filePath);
+        try {
+            files = readdir(filePath);
+        } catch (err) {
+            if (err.errno === 34) {
+                console.log('Path does not exist');
+            } else {
+                throw err;
+            }
+        }
     }
+
+    console.log(files);
 };
 
 const init = () => {
