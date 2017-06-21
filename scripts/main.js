d79c35ceccbb0c4924bd922b1aefc260cf61df1f
Gabor Szalay <yaastreet@gmail.com>
2017-06-21 17:53:33 +0200

    deleting second window onload

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import Main from '../views/main.jsx';
 
 window.onload = function () {
-
     ReactDOM.render(
         <Main />,
         document.getElementById('root')
--- server/renderer.js
+++ server/renderer.js
@@ -2,9 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-window.onload = function () {
-    require.main.require('./server/router.js').init();
-    require.main.require('./server/series-list-renderer.js').init();
-    require.main.require('./server/vlc-path-controller.js').init();
-    require.main.require('./server/episode-path-controller.js').init();
-}
+require.main.require('./server/router.js').init();
+require.main.require('./server/series-list-renderer.js').init();
+require.main.require('./server/vlc-path-controller.js').init();
+require.main.require('./server/episode-path-controller.js').init();
