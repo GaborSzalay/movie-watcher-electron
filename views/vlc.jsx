d76456cea2c1d07986fda5dce62ffbb5aeafff6e
GaborSzalay <yaastreet@gmail.com>
2017-06-26 23:14:13 +0200

    adding vlc path to state

--- views/vlc.jsx
+++ views/vlc.jsx
@@ -3,11 +3,29 @@
 import React from 'react';
 
 export default class VlcSetup extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { vlcPath: false };
+        this.handleVlcInputChange = this.handleVlcInputChange.bind(this);
+    }
+
+
+    handleVlcInputChange(event) {
+        const filePath = event.nativeEvent.path[0].files[0].path;
+
+        this.setState(() => ({
+            vlcPath: filePath
+        }));
+    };
+
     render() {
         return (
-            <h1>
-                VLC!
-            </h1>
+            <div>
+                <label >Select VLC player:</label>
+                <input type="file" id="vlc-path" name="vlc-path" onChange={this.handleVlcInputChange}/><br />
+                <span id="vlc-path-container">{this.state.vlcPath}</span>
+            </div>
         );
     }
 }
