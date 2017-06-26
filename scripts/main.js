9ab625900e2851d9aa40c84a240a65994c1523a6
Gabor Szalay <yaastreet@gmail.com>
2017-06-26 17:51:13 +0200

    adding app component and using BrowserRouter

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from '../views/main.jsx';
+import { BrowserRouter } from 'react-router-dom'
+import App from '../views/app.jsx';
 
 window.onload = function () {
     ReactDOM.render(
-        <Main />,
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>,
         document.getElementById('root')
     );
-
 }
---
+++ views/app.jsx
@@ -0,0 +1,10 @@
+'use babel';
+
+import React from 'react';
+import Main from './main.jsx';
+
+export default class App extends React.Component {
+    render() {
+        return <Main />
+    }
+}
